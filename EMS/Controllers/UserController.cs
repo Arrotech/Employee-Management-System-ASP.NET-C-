b0c00661fd04125238c6a0b1d42d5f80a5fb6eba
Harun Gachanja <arrotechdesign@gmail.com>
2021-05-12 17:00:25 +0300

    set up

---
+++ EMS/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using EMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EMS.Controllers
+{
+    public class UserController : ApiController
+    {
+
+        // GET: api/User
+        public IEnumerable<Employee> Get()
+        {
+            using (EmployeeDBContext dbContext = new EmployeeDBContext())
+            {
+                var users = dbContext.Employees.ToList();
+                return users;
+            }
+        }
+
+        // GET: api/User/5
+        public Employee Get(int id)
+        {
+            using (EmployeeDBContext dbContext = new EmployeeDBContext())
+            {
+                var user = dbContext.Employees.FirstOrDefault(e => e.UserId == id);
+                return user;
+            }
+        }
+
+        // POST: api/User
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/User/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/User/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
